5c612dd89630c26d808c7e69a1a92a46298619f8
Franklin Martinez <fml010603@gmail.com>
2021-04-20 13:32:36 -0500

    se agrego la libreria type.js

--- tacos-app/js/index.js
+++ tacos-app/js/index.js
@@ -7,26 +7,34 @@ const titulo = document.querySelector('#titulo');
 const contenedor_login = document.querySelector('.login_contenedor');
 const contenedor_registro = document.querySelector('.registro_contenedor');
 
+//======================================
+// 	EVENTOS
+//======================================
+
+
 login_normal.addEventListener('click', (e) => {
     e.preventDefault();
 
-    if(user.value === '' || pass.value === ''){
-        alerta('error', 'Los campos son obligatorios')
-        return;
-    }
+    if (errorInputs() === 0) {
 
-    const data = {
-        user: user.value,
-        pass: pass.value
-    }
+        const data = {
+            user: user.value,
+            pass: pass.value
+        }
+        document.querySelector('.formulario').reset();
+        alerta('success', 'Inicio de sesión correcto');
 
-    alerta('success', 'Inicio de sesión correcto');
+
+    }
 });
+
 
 login_google.addEventListener('click', (e) => {
 
     e.preventDefault();
     alerta('success', 'Inicio de sesión correcto');
+    removeClassInput('pass')
+    removeClassInput('user')
 });
 
 link.addEventListener('click', () => {
@@ -36,7 +44,19 @@ link.addEventListener('click', () => {
     }, 1000);
 });
 
-const alerta = ( icon, title) => {
+pass.addEventListener('focus', () => {
+    removeClassInput('pass')
+});
+
+user.addEventListener('focus', () => {
+    removeClassInput('user')
+});
+
+//======================================
+// 	 FUNCIONES
+//======================================
+
+const alerta = (icon, title) => {
     const Toast = Swal.mixin({
         toast: true,
         position: 'top-end',
@@ -44,13 +64,46 @@ const alerta = ( icon, title) => {
         timer: 3000,
         timerProgressBar: true,
         didOpen: (toast) => {
-          toast.addEventListener('mouseenter', Swal.stopTimer)
-          toast.addEventListener('mouseleave', Swal.resumeTimer)
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
         }
-      })
-      
-      Toast.fire({
+    })
+
+    Toast.fire({
         icon,
         title
-      })
+    })
+};
+
+const errorInputs = () => {
+
+    const pass_txt = document.querySelector('.pass');
+    const user_txt = document.querySelector('.user');
+
+    let errores = 0;
+
+    if (user.value === '') {
+        user_txt.children[1].classList.add("icono_error")
+        user_txt.children[0].classList.add("error_input_input")
+        user_txt.classList.add("erorr_input");
+        errores += 1;
+    }
+
+    if (pass.value === '') {
+        pass_txt.children[1].classList.add("icono_error")
+        pass_txt.children[0].classList.add("error_input_input")
+        pass_txt.classList.add("erorr_input");
+        errores += 1;
+    }
+
+    (errores !== 0) && alerta('error', 'Los campos son obligatorios');
+
+    return errores;
+};
+
+const removeClassInput = (tipo) => {
+    const txt = document.querySelector(`.${tipo}`);
+    txt.children[1].classList.remove("icono_error")
+    txt.children[0].classList.remove("error_input_input")
+    txt.classList.remove("erorr_input");
 };
